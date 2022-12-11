759e9d2b9a8973ccbad7adafb22cb81494fddca8
tmin002 <ssh9930@gmail.com>
2022-12-11 16:49:15 +0900

    프로토콜 대로 UTF-8에서 ASCII 로 수정

--- core/src/main/java/kr/gagaotalk/core/Constants.java
+++ core/src/main/java/kr/gagaotalk/core/Constants.java
@@ -5,6 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
-    public final static byte[] HEADER_STRING_BYTES = "gagaotalk!".getBytes(StandardCharsets.UTF_8);
+    public final static byte[] HEADER_STRING_BYTES = "gagaotalk!".getBytes(StandardCharsets.US_ASCII);
     public final static Map<String, Object> EMPTY_MAP = new HashMap<>();
 }
