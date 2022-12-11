a77a7ba1c1512e374920ef6913504fb8c6ff3596
tmin002 <ssh9930@gmail.com>
2022-12-11 19:21:47 +0900

    최상위 JFrame (Window) 추가

--- client/src/main/java/kr/gagaotalk/client/gui/window/Window.java
+++ client/src/main/java/kr/gagaotalk/client/gui/window/Window.java
@@ -1,6 +1,17 @@
 package kr.gagaotalk.client.gui.window;
 
-import javax.swing.*;
+import kr.gagaotalk.client.gui.ResourceManager;
 
+import javax.swing.*;
+import java.awt.*;
+
+// 프로그램의 모든 창에 적용되는 코드
 public class Window extends JFrame {
+    public Window() {
+        setTitle("gagaotalk");
+        setIconImage(ResourceManager.getImageIcon("/gagaotalk_icon.png").getImage());
+        setBackground(Color.white);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
 }
