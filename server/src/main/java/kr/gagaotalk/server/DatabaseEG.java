de89338e19c36fa19ed7cfecf0333531825ab6c4
jwooo02 <jwooooy002@gmail.com>
2022-12-12 23:50:07 +0900

    ChatPanel 및 기타 수정 뿌우우ㅜ우우직 푸드득 뿡빵

--- server/src/main/java/kr/gagaotalk/server/DatabaseEG.java
+++ server/src/main/java/kr/gagaotalk/server/DatabaseEG.java
@@ -4,6 +4,8 @@ import sun.security.x509.FreshestCRLExtension;
 
 public class DatabaseEG {
     public static void main(String args[]) {
-
+        /*UserTable user1 = new UserTable(con, "usertable");
+        user1.makeTable();*/
+        // 뿌직
     }
 }
