e5d0f0db5194d4b06f5d0d855faebaec71d58834
jwooo002 <jwooooy002@gmail.com>
2022-12-13 22:26:14 +0900

    add class file

---
+++ server/src/main/java/kr/gagaotalk/server/table/FriendsTables.java
@@ -0,0 +1,33 @@
+package kr.gagaotalk.server.table;
+
+import java.sql.Connection;
+
+public class FriendsTables extends Table {
+    FriendsTables(Connection con) { super(con); }
+    FriendsTables(Connection con, String userID) { super(con, userID + "_friends", schema, database); }
+
+    public static String database = "friends";
+    public static String schema = "id varchar(32) not null, primary key(id)"; // or "id varchar(32) not null, state varchar(8), primary key(id)" state : invisible/ block etc.
+
+
+
+    public boolean isFriend(String searchID) {
+        StringBuilder countS = executeQuery("select exists ( select from " + tableName + " where id = '" + searchID + "') as success;", 1);
+        return countS.toString().equals("1");
+    }
+
+    public int getNumberOfFriends() {
+        StringBuilder countS = executeQuery("select count(*) from " + tableName + ";", 1);
+        int count = Integer.parseInt(countS.toString());
+        return count;
+    }
+
+    public StringBuilder getFriends() {
+        StringBuilder friends = executeQuery("select id from " + tableName + ";", 1, '\n');
+        return friends;
+    }
+
+
+
+
+}
